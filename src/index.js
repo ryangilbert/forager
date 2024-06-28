3db9f4219c9d5b8a259f087a1baab59ad467eba2
Ryan Gilbert <ryanbgilbert@gmail.com>
2024-06-27 19:44:07 -0700

    Finish webpack asset-management tutorial and leave in image and css loaders

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import _ from 'lodash';
+import './style.css';
+import Leaf from './leaf.jpeg';
 
+import { createRoot } from 'react-dom/client';
+
+// from React tutorial
+
+// // Clear the existing HTML content
+// document.body.innerHTML = '<div id="app"></div>';
+
+// // Render your React component instead
+// const root = createRoot(document.getElementById('app'));
+// root.render(<h1>Hello, world</h1>);
+
+// from webpack tutorial
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myLeaf = new Image();
+  myLeaf.src = Leaf;
+
+  element.appendChild(myLeaf);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,41 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+              ['@babel/preset-react'],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
   mode: 'development',
 };
