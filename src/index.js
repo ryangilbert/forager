90a6dae2d190bfa4b47acd11a71b3a756f07c6d6
Ryan Gilbert <ryanbgilbert@gmail.com>
2024-06-28 09:19:46 -0700

    Render a react component within my html component from index.js

--- src/index.js
+++ src/index.js
@@ -5,36 +5,49 @@ import printMe from './print.js';
 
 import { createRoot } from 'react-dom/client';
 
-// from webpack tutorial
-// function component() {
-//   const element = document.createElement('div');
-//   const btn = document.createElement('button');
-
-//   // Lodash, now imported by this script
-//   element.innerHTML = _.join(['Hello', 'webpack and Ryan'], ' ');
-//   element.classList.add('hello');
-//   btn.innerHTML = 'Click me and check the console!';
-//   btn.onclick = printMe;
-
-//   element.appendChild(btn);
-
-//   // Add the image to our existing div.
-//   const myLeaf = new Image();
-//   myLeaf.src = Leaf;
-
-//   element.appendChild(myLeaf);
-
-//   return element;
-// }
-
-// document.body.appendChild(component());
-
 // from React tutorial
 
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
+// from webpack tutorial
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-// // Render your React component instead
-const root = createRoot(document.getElementById('app'));
-// console.log('..............HELLO....................');
-root.render(<h1>Hello, from React and Ryan</h1>);
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack and Ryan'], ' ');
+  element.classList.add('hello');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  // Add the image to our existing div.
+  //   const myLeaf = new Image();
+  //   myLeaf.src = Leaf;
+
+  //   element.appendChild(myLeaf);
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+// For testing React: https://react.dev/learn/add-react-to-an-existing-project#using-react-for-a-part-of-your-existing-page
+
+// Create nav element
+function navComponent() {
+  const element = document.createElement('nav');
+  element.id = 'navigation';
+
+  return element;
+}
+// Append nav element to doc body
+document.body.appendChild(navComponent());
+
+// Find nav element and we will render a React component inside of it
+function NavigationBar() {
+  // TODO: Actually implement a navigation bar
+  return <h1>Hello from React!</h1>;
+}
+const domNode = document.getElementById('navigation');
+const root = createRoot(domNode);
+root.render(<NavigationBar />);
