77194bed692961b903d7e72d2e1207be906dfd64
Ryan Gilbert <ryanbgilbert@gmail.com>
2024-06-27 20:08:24 -0700

    Finish webpack output-management tutorial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css';
 import Leaf from './leaf.jpeg';
+import printMe from './print.js';
 
 import { createRoot } from 'react-dom/client';
 
@@ -16,10 +17,15 @@ import { createRoot } from 'react-dom/client';
 // from webpack tutorial
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   // Add the image to our existing div.
   const myLeaf = new Image();
