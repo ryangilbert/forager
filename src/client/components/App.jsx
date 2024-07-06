a9f4ba65aaf218b8abebd205949e7ebff915b718
Ryan Gilbert <ryanbgilbert@gmail.com>
2024-07-06 12:03:41 -0700

    Location search working

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -3,85 +3,18 @@ import Search from './Search.jsx';
 import RestroomList from './RestroomList.jsx';
 
 const App = () => {
+  const [inputValue, setInputValue] = useState('');
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [address, setAddress] = useState(null);
 
-  function getLatLong() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, error);
-    } else {
-      console.log('Geolocation not supported');
-    }
+  function handleSearch(event) {
+    console.log('back here');
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const input = formData.get('query');
+    setInputValue(input);
   }
-
-  function success(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    setLatitude(latitude);
-    setLongitude(longitude);
-  }
-
-  function error() {
-    console.log('Unable to retrieve your location');
-  }
-
-  useEffect(() => {
-    getLatLong();
-  });
-
-  // Reverse geocode to get location
-
-  // useEffect(() => {
-  //   if (latitude && longitude) {
-  //     // OpenCage code
-  //     const opencage_api_key = process.env['OPENCAGE_API_KEY'];
-  //     const query = latitude + ',' + longitude;
-  //     const api_url = 'https://api.opencagedata.com/geocode/v1/json';
-  //     const request_url =
-  //       api_url +
-  //       '?' +
-  //       'key=' +
-  //       opencage_api_key +
-  //       '&q=' +
-  //       encodeURIComponent(query) +
-  //       '&pretty=1' +
-  //       '&no_annotations=1';
-  //     // TODO: Refactor to use Fetch API
-  //     var request = new XMLHttpRequest();
-  //     request.open('GET', request_url, true);
-
-  //     request.onload = function () {
-  //       // see full list of possible response codes:
-  //       // https://opencagedata.com/api#codes
-
-  //       if (request.status === 200) {
-  //         // Success!
-  //         const data = JSON.parse(request.responseText);
-  //         const postcode = data.results[0].components.postcode;
-  //         // alert(data.results[0].formatted); // print the location
-  //         const address = data.results[0].formatted;
-  //         setAddress(address);
-  //         // console.log('results....', data.results[0].formattted);
-  //       } else if (request.status <= 500) {
-  //         // We reached our target server, but it returned an error
-
-  //         console.log('unable to geocode! Response code: ' + request.status);
-  //         const data = JSON.parse(request.responseText);
-  //         console.log('error msg: ' + data.status.message);
-  //       } else {
-  //         console.log('server error');
-  //       }
-  //     };
-
-  //     request.onerror = function () {
-  //       // There was a connection error of some sort
-  //       console.log('unable to connect to server');
-  //     };
-
-  //     request.send(); // make the request
-  //   }
-  // }, [latitude, longitude, getLatLong]);
 
   return (
     <div>
@@ -93,7 +26,14 @@ const App = () => {
         <br></br>
       </div>
       {/* <LocationSearch /> */}
-      <Search />
+      <Search
+        handleSearch={handleSearch}
+        inputValue={inputValue}
+        latitude={latitude}
+        longitude={longitude}
+        setLatitude={setLatitude}
+        setLongitude={setLongitude}
+      />
       <div>
         {latitude}
         {longitude}
--- src/client/components/Search.jsx
+++ src/client/components/Search.jsx
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
-export default function Search() {
-  const [inputValue, setInputValue] = useState('');
+export default function Search(props) {
+  // const [inputValue, setInputValue] = useState('');
+  // const [latitude, setLatitude] = useState(null);
+  // const [longitude, setLongitude] = useState(null);
   const [location, setLocation] = useState('');
 
-  function search(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const input = formData.get('query');
-    setInputValue(input);
-  }
+  // function handleSearch(event) {
+  //   event.preventDefault();
+  //   const formData = new FormData(event.target);
+  //   const input = formData.get('query');
+  //   setInputValue(input);
+  // }
 
   useEffect(() => {
-    console.log('Search input:', inputValue);
+    console.log('Search input:', props.inputValue);
   });
 
   useEffect(() => {
-    if (inputValue) {
+    if (props.inputValue) {
       // OpenCage code
       const opencage_api_key = process.env['OPENCAGE_API_KEY'];
-      const query = inputValue;
+      const query = props.inputValue;
       const api_url = 'https://api.opencagedata.com/geocode/v1/json';
       const request_url =
         api_url +
@@ -42,18 +44,26 @@ export default function Search() {
           }
         })
         .then((data) => {
-          console.log('data', data);
+          console.log('data', data.results[0].geometry);
+          props.setLatitude(data.results[0].geometry.lat);
+          props.setLongitude(data.results[0].geometry.lng);
         })
         .catch((error) => {
           console.error('Error:', error.message);
         });
     }
-  }, [inputValue]);
+  }, [props.inputValue]);
 
   return (
-    <form onSubmit={search}>
-      <input name="query" />
-      <button type="submit">Search</button>
-    </form>
+    <div>
+      <form onSubmit={props.handleSearch}>
+        <input name="query" />
+        <button type="submit">Search</button>
+      </form>
+      {/* <div>
+        {latitude}
+        {longitude}
+      </div> */}
+    </div>
   );
 }
